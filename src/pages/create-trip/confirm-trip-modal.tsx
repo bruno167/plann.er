fafa721b76505551e4e5e54988181038eb2cceda
Bruno <brunoa0504@gmail.com>
2024-07-10 19:47:12 -0300

    refactor: buttons into components

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,5 +1,6 @@
 import { User, X } from "lucide-react";
 import { FormEvent } from "react";
+import { Button } from "../../components/button";
 
 interface ConfirmTripModalProps {
     closeConfirmTripModal: () => void
@@ -48,9 +49,9 @@ export function ConfirmTripModal({
                 />
               </div>
 
-              <button type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <Button type="submit" variant="primary" size="full" >
                 Confrimar criação da viagem
-              </button>
+              </Button>
             </form>
           </div>
         </div>
