37e9df338766e7b77b6aef8fae9b85c5980770dc
Bruno <brunoa0504@gmail.com>
2024-07-10 23:54:53 -0300

    feat: texts fixes

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -13,8 +13,8 @@ export function Activities() {
 
             <div className="space-y-2.5">
                 <div className="flex gap-2 items-baseline">
-                    <span className="text-xl text-zinc-300 font-semibold">dia 17</span>
-                    <span className="text-xs text-zinc-500">Sábado</span>
+                    <span className="text-xl text-zinc-300 font-semibold">dia 18</span>
+                    <span className="text-xs text-zinc-500">Domingo</span>
                 </div>
                 <div className="space-y-2.5">
                     <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
